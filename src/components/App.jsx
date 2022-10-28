810e79529933994dbd8d1ea8085a1270a23f0528
Muwaheed <muwaheedmustapha@gmail.com>
2022-10-28 22:30:45 +0100

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom"
+import Landing  from "./Landing.jsx"
+import DashBoard from "./DashBoard.jsx"
+import LeaderBoard from "./LeaderBoard.jsx"
+
+function App() {
+    return(
+        <div>
+            <Landing />
+        </div>
+    )
+
+}
+
+
+export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+
+function Landing(){
+    return(
+        <Navigation/>
+    )
+
+}
+
+
+export default Landing
