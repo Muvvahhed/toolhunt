10e2a8b821ca5cd37344c35e0d3a4bdac076a795
Muwaheed <muwaheedmustapha@gmail.com>
2022-10-31 08:48:27 +0100

    Updates

---
+++ precache-manifest.33222879ca4f0497919ccdce87599f0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ee798176ca4fddbb56",
+    "url": "/toolhunt/static/css/main.d975c855.chunk.css"
+  },
+  {
+    "revision": "d2ee798176ca4fddbb56",
+    "url": "/toolhunt/static/js/main.d2ee7981.chunk.js"
+  },
+  {
+    "revision": "34ad1929c434dc70d787",
+    "url": "/toolhunt/static/css/1.c2eb62c2.chunk.css"
+  },
+  {
+    "revision": "34ad1929c434dc70d787",
+    "url": "/toolhunt/static/js/1.34ad1929.chunk.js"
+  },
+  {
+    "revision": "1cd16efa274c4cb7b473",
+    "url": "/toolhunt/static/js/runtime~main.1cd16efa.js"
+  },
+  {
+    "revision": "85c4f88a9c63d3138bce1893e0f33034",
+    "url": "/toolhunt/index.html"
+  }
+];
