88d3ac6455673d4a2c41ddb257d3e74f35a84d78
Muwaheed <muwaheedmustapha@gmail.com>
2022-10-31 08:34:21 +0100

    Add comments to App Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
 import React, {useState} from "react";
-import Landing  from "./Landing.jsx"
-import DashBoard from "./DashBoard.jsx"
-import LeaderBoard from "./LeaderBoard.jsx"
-import Navigation from "./Navigation";
+import Landing  from "./Landing.jsx" // landing page component
+import DashBoard from "./DashBoard.jsx" // dashboard component
+import LeaderBoard from "./LeaderBoard.jsx" // leaderboard page component
+import Navigation from "./Navigation"; // navigation bar component
 
 function App() {
-    const[page, setPage] = useState("landing")
+    const[page, setPage] = useState("landing") //sets initial page to the landing page
     function currentPage(page){
         setPage(page)
     }
     return(
         <div>
             <Navigation currentpage={currentPage} />
-            {/**/}
+            {/*conditional rendering of pages as the page state changes*/}
             {page === "landing" && <Landing/>}
             {page === "dashBoard" && <DashBoard/>}
             {page === "leaderBoard" && <LeaderBoard/>}
